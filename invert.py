28d551470489c1bf4fea4cf96777fe1c9a228a4e
Justino <justinaverysmith@gmail.com>
2019-10-02 01:56:12 -0500

    delted stuf

--- invert.py
+++
@@ -1,7 +0,0 @@
-
-from PIL import Image 
-import PIL.ImageOps
-
-image = Image.open("Emilia-Clark.jpg")
-inverted_image = PIL.ImageOps.invert(image)
-inverted_image.save("Emilia-Clark-inverted.png")
